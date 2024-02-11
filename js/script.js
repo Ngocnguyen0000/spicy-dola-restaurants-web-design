d2b84328b123f7cd1bb2495b3b17d745caf868e7
Bich Ngoc Dev <2ndevdes@gmail.com>
2024-02-11 13:06:18 +0700

    fix and home section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-window.onclick = () => {
-    menu.classList.remove( 'fa-times' );
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
 
@@ -17,6 +17,20 @@ document.querySelector('#search-icon').onclick = () => {
 
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
+    menu.classList.remove('fa-times'); // Đảm bảo biểu tượng menu được chuyển trạng thái trở lại khi đóng hộp tìm kiếm
 }
 
 
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
