6bf78fad710554aa09c6f0dc016a3ddcc9524290
Bich Ngoc Dev <2ndevdes@gmail.com>
2024-02-14 00:26:28 +0700

    review and fit

--- js/script.js
+++ js/script.js
@@ -21,10 +21,11 @@ document.querySelector('#close').onclick = () => {
 
 
 var swiper = new Swiper(".home-slider", {
-  slidesPerView: 1,
   spaceBetween: 30,
-  keyboard: {
-    enabled: true
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
   },
   pagination: {
     el: ".swiper-pagination",
@@ -32,3 +33,31 @@ var swiper = new Swiper(".home-slider", {
   },
   loop: true
 });
+
+
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false
+  },
+  loop: true,
+  slidesPerView: 3, // Show 3 slides per view by default
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+          slidesPerView: 2,
+      },
+      768: {
+          slidesPerView: 2,
+      },
+      1024: {
+          slidesPerView: 3,
+      },
+  }
+});
+
