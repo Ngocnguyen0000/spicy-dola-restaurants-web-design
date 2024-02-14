34794564d33df8677584a0a8e914248ad6d85961
Bich Ngoc Dev <2ndevdes@gmail.com>
2024-02-15 00:09:15 +0700

    huhu bug slider home and loader and review slide

--- js/script.js
+++ js/script.js
@@ -6,9 +6,29 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top => offset && top <offset + height)
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+
+    })
+
+    })
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -61,3 +81,14 @@ var swiper = new Swiper(".review-slider", {
   }
 });
 
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+    clearInterval(intervalId); // Clear the interval after the fade-out animation
+}
+
+function fadeOut() {
+    intervalId = setInterval(loader, 3000); // Store the interval ID
+}
+
+window.onload = fadeOut;
